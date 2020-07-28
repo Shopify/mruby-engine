5edbb518520763f4639825759c6435af50124ff9
Jean Boussier <jean.boussier@gmail.com>
2020-07-28 09:20:14 +0200

    Only set -Werror on own CI

--- ext/mruby_engine/extconf.rb
+++ ext/mruby_engine/extconf.rb
@@ -35,6 +35,6 @@ end
 $CFLAGS << ' -std=gnu99 -fvisibility=hidden -Wno-declaration-after-statement '
 $CFLAGS << " #{Flags.cflags.join(' ')} "
 $CFLAGS << " #{Flags.defines.map { |define| "-D#{define}" }.join(' ')} "
-$CFLAGS << " -Werror " if ENV["CI"]
+$CFLAGS << " -Werror " if ENV["W_ERROR"]
 
 create_makefile 'mruby_engine/mruby_engine'
