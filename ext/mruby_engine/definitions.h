2ded279c94a8b611e2f09bb8c997c8b34bf1b084
Randy Stauner <randy.stauner@shopify.com>
2026-09-04 12:13:32 -0700

    Define ME_ADDRESS_SANITIZER for later use

--- ext/mruby_engine/definitions.h
+++ ext/mruby_engine/definitions.h
@@ -8,4 +8,12 @@
 #define KiB 1024
 #define MiB (1024 * KiB)
 
+#if defined(__SANITIZE_ADDRESS__) // gcc
+#  define ME_ADDRESS_SANITIZER 1
+#elif defined(__has_feature) // clang
+#  define ME_ADDRESS_SANITIZER __has_feature(address_sanitizer)
+#else
+#  define ME_ADDRESS_SANITIZER 0
+#endif
+
 #endif
